a0712906c96515cdaefad32608543c37b49648d6
Jeff Zhao <somos800-787@Somos800-787s-MacBook-Pro.local>
2020-08-25 23:24:31 -0400

    updated files

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,23 @@
-docker build -t jzhao5/multi-client:latest jzhao5/multi-client:$SHA -f ./client/Dockerfile ./client
-docker build -t jzhao5/multi-api:latest jzhao5/multi-api:$SHA -f ./server/Dockerfile ./server
-docker build -t jzhao5/multi-worker:latest jzhao5/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t jzhao5/multi-client:latest jzhao5/multi-client2:$SHA -f ./client/Dockerfile ./client
+docker build -t jzhao5/multi-api:latest jzhao5/multi-server2:$SHA -f ./server/Dockerfile ./server
+docker build -t jzhao5/multi-worker:latest jzhao5/multi-worker2:$SHA -f ./worker/Dockerfile ./worker
 
-docker push jzhao5/multi-client:latest
-docker push jzhao5/multi-api:latest
-docker push jzhao5/multi-worker:latest
+docker push jzhao5/multi-client2:latest
+docker push jzhao5/multi-server2:latest
+docker push jzhao5/multi-worker2:latest
 
-docker push jzhao5/multi-client:$SHA
-docker push jzhao5/multi-api:$SHA
-docker push jzhao5/multi-worker:$SHA
+docker push jzhao5/multi-client2:$SHA
+docker push jzhao5/multi-server2:$SHA
+docker push jzhao5/multi-worker2:$SHA
+
+export AWS_ACCESS_KEY_ID="$(echo ${CREDENTIALS} | jq -r '.Credentials.AccessKeyId')"
+export AWS_SECRET_ACCESS_KEY="$(echo ${CREDENTIALS} | jq -r '.Credentials.SecretAccessKey')"
+##export AWS_SESSION_TOKEN="$(echo ${CREDENTIALS} | jq -r '.Credentials.SessionToken')"
+##export AWS_EXPIRATION=$(echo ${CREDENTIALS} | jq -r '.Credentials.Expiration')
+aws eks update-kubeconfig --name $EKS_CLUSTER_NAME
 
 kubectl apply -f k8s --namespace=multi-k8s
 
-kubectl set image deployments/client-deployment client=jzhao5/multi-client:$SHA
-kubectl set image deployments/api-deployment server=jzhao5/multi-api:$SHA
-kubectl set image deployments/worker-deployment worker=jzhao5/multi-worker:$SHA
+kubectl set image deployments/client-deployment client=jzhao5/multi-client2:$SHA
+kubectl set image deployments/server-deployment server=jzhao5/multi-server2:$SHA
+kubectl set image deployments/worker-deployment worker=jzhao5/multi-worker2:$SHA
